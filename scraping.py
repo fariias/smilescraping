6a96a57c2ae31eec6ab83a61a98875722ccc4ee6
juniorfarias <junior@unhideschool.com>
2019-10-17 14:00:44 -0300

    Added: selenium webdriver.

--- scraping.py
+++ scraping.py
@@ -1,26 +1,38 @@
 from bs4 import BeautifulSoup
-import requests
-
+from selenium import webdriver
+from furl import furl
+import chromedriver_binary
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 def request_from_url(url: str, params: dict = {}) -> dict():
-    req = requests.get(url, params=params)
-    content = {}
 
-    if req.status_code == 200:
-        print('Success!')
-        content = req.content
+    f_url = furl(url)
+    f_url.args = params
 
-    return content
+    options = webdriver.ChromeOptions()
+    options.add_argument("javascript.enabled")
+    options.add_argument("--enable-javascript")
+
+    browser = webdriver.Chrome('chromedriver', options=options)
+    browser.get(f_url.url)
+
+    element = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, '//*[@id="alertModalLoading"]')))
+
+    html = browser.page_source
+
+    return html
 
 
-def process_data(content: str) -> BeautifulSoup:
-    soup = BeautifulSoup(content, 'html.parser')
+def process_data_with_soap(content: str) -> BeautifulSoup:
+    soup = BeautifulSoup(content, 'lxml')
 
     return soup
 
 
 def search_flights():
-    url = 'https://www.smiles.com.br/home'
+    url = 'https://www.smiles.com.br/emissao-com-milhas'
 
     params = {
         'adults': 1,
@@ -30,15 +42,16 @@ def search_flights():
         'tripType': 1,
         'originAirport': 'RIO',
         'destinationAirport': 'NAT',
-        'departureDate': 1571403600000,
-        'returnDate': 1572008400000
-
+        'departureDate': 1576760400000,
+        'returnDate': 1578056400000
     }
 
-    request = request_from_url(url)
-    processed_data = process_data(request)
+    request = request_from_url(url, params)
+    soup = process_data_with_soap(request)
 
-
+    first_flights = soup.find_all(id='firstFlights')
+    for flight in first_flights:
+        print(flight)
 
 
 
